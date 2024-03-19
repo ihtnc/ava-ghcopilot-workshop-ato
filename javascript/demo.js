39accb5562e97460eaa1e9aece9fad862679490d
Alvin Vinalon <alvin.vinalon@avanade.com>
2024-03-19 13:19:45 +1100

    Added demo.js file

---
+++ javascript/demo.js
@@ -0,0 +1,8 @@
+// Generate a function that will accept a string and return the string in reverse
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+// Test the function
+console.log(reverseString('Alvin')); // olleh
