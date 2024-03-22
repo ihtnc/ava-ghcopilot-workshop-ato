e16ebc1c734b3a127fc7762e9b75197a66f0d123
Alvin Vinalon <alvin.vinalon@avanade.com>
2024-03-22 13:52:19 +1100

    Add memoized Fibonacci function and update comments

--- complete/javascript/demo.js
+++ complete/javascript/demo.js
@@ -5,4 +5,35 @@ function reverseString(str) {
 }
 
 // Test the function
-console.log(reverseString('Alvin')); // olleh
+// console.log(reverseString('Alvin')); // olleh
+
+
+// Chat: Implement a memoized function to calculate Fibonacci numbers to improve performance
+
+const memoizedFibonacci = (function() {
+  let memo = {};
+
+  function f(n) {
+    let value;
+
+    if (n in memo) {
+      value = memo[n];
+    } else {
+      if (n === 0 || n === 1)
+        value = n;
+      else
+        value = f(n - 1) + f(n - 2);
+
+      memo[n] = value;
+    }
+
+    return value;
+  }
+
+  return f;
+})();
+
+console.log(memoizedFibonacci(10));  // 55
+console.log(memoizedFibonacci(20)); // 6765
+
+
--- exercise/python/demo.py
+++ exercise/python/demo.py
@@ -1,1 +1,5 @@
+# Q: What is an API?
+# A: 
+
+
 # Generate a regular expression that checks if a text is a valid URL or not.
