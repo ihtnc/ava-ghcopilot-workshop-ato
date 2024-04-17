7b6025f1a9d6cb161c8ea39ddabb968b42b4c665
Alvin Vinalon <alvin.vinalon@avanade.com>
2024-04-17 21:55:05 +1000

    Refactor JavaScript files and update tour titles

--- complete/javascript/demo.js
+++ complete/javascript/demo.js
@@ -13,36 +13,26 @@ function performOperation(num1, num2, operator) {
   }
 }
 
-module.exports = { performOperation };
-
-// Generate a function that will accept a string and return the string in reverse
-
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
 
-// Test the function
-// console.log(reverseString('Alvin')); // olleh
-
-// Chat: Implement a memoized function to calculate Fibonacci numbers to improve performance
-
-const memoizedFibonacci = (function () {
-  let memo = {};
-
-  function f(n) {
-    let value;
-
-    if (n in memo) {
-      value = memo[n];
-    } else {
-      if (n === 0 || n === 1) value = n;
-      else value = f(n - 1) + f(n - 2);
-
-      memo[n] = value;
-    }
-
-    return value;
+function memoizedFibonacci(n, memo = {}) {
+  if (n in memo) {
+    return memo[n];
   }
 
-  return f;
-})();
+  if (n <= 1) {
+    return n;
+  }
+
+  memo[n] = memoizedFibonacci(n - 1, memo) + memoizedFibonacci(n - 2, memo);
+  return memo[n];
+}
+
+// Export all the functions for testing
+module.exports = {
+  performOperation,
+  reverseString,
+  memoizedFibonacci,
+};
--- exercise/javascript/demo.js
+++ exercise/javascript/demo.js
@@ -1,8 +1,28 @@
-// Generate a function that will accept a string and return the string in reverse
+// Generate a function that will accept two numbers and an arithmetic operator. The function will perform the operation between the numbers and return the result.
 
-function reverseString(str) {
-  return str.split("").reverse().join("");
+function arithmetic(num1, num2, operator) {
+  try {
+    if (isNaN(num1) || isNaN(num2)) {
+      throw new Error("Invalid numbers");
+    }
+    return eval(num1 + operator + num2);
+  } catch (error) {
+    return "Invalid operator or numbers";
+  }
 }
 
-console.log(reverseString("hello")); // olleh
-console.log(reverseString("world")); // dlrow
+// Test the function by accepting user input for the numbers and the operator using Node Readline.
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("Enter the first number: ", (num1) => {
+  readline.question("Enter the second number: ", (num2) => {
+    readline.question("Enter the operator: ", (operator) => {
+      console.log(arithmetic(num1, num2, operator));
+      readline.close();
+    });
+  });
+});
